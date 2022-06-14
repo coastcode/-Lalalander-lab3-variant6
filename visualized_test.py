860c67b9a93839281a3daa6a05d7071ef0e3b791
coastcode <81280412+coastcode@users.noreply.github.com>
2022-06-14 23:40:46 +0800

    Create visualized_test.py

---
+++ visualized_test.py
@@ -0,0 +1,19 @@
+import unittest
+import graphviz
+from visualized import *
+
+
+class FactorialTest(unittest.TestCase):
+    def test_Factorial(self):
+        # 阶乘
+        Lbd = Lambda()
+        self.assertEqual(Lbd.FACT(5), 120)
+        self.assertEqual(Lbd.FACT(8), 40320)
+        dot = Lbd.visualize()
+        f = open('fsm.dot', 'w')
+        f.write(dot)
+        f.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
